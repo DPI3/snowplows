2b1eb7907b6c106160fa00c28112655f5199913a
Marton Toth <toth.marton21@gmail.com>
2026-03-29 13:56:31 +0200

    Test 8 fixed

--- skeleton/tests/test8.java
+++ skeleton/tests/test8.java
@@ -1,56 +1,32 @@
 package skeleton.tests;
 
-import java.util.Scanner;
 import skeleton.src.*;
 
 /**
  * 8. teszteset: Takarítás hányó fejjel teszt.
  *
  * A teszteset ellenőrzi, hogy a hókotró hányó fejjel megfelelően takarítja az utat.
- * A CleanerRole a hókotrót a takarítani kívánt útszakaszra irányítja, majd a hókotró
- * a hányó fej segítségével megpróbálja letakarítani az adott útszakaszt.
- *
- * A teszt során az alábbi esetek vizsgálhatók:
- * - havas útszakasz
- * - jégpáncélos útszakasz
- * - feltört jeges útszakasz
- *
- * Elvárt működés:
- * - jégpáncél esetén a hányó fej nem tud takarítani,
- * - hó vagy feltört jég esetén az út megtisztul,
- * - a CleanerRole jutalmat kap a sikeres takarításért.
  */
 public class Test8 implements TestCase {
 
     /**
      * A tesztszekvencia futtatása.
-     *
-     * A teszt létrehozza a szükséges objektumokat, majd végrehajtja a
-     * hányó fejjel történő takarítás folyamatát.
-     *
-     * @param scanner a scanner objektum a felhasználói bevitel olvasásához
      */
     @Override
-    public void run(Scanner scanner) {
-        Skeleton.printCall("Test8", "run(scanner)");
+    public void run() {
 
-        Game game = new Game();
+        Skeleton.printCall("Test8", "run()");
+
         CleanerRole cleanerRole = new CleanerRole();
         Lane lane = new Lane();
         ThrowerHead throwerHead = new ThrowerHead();
         Snowplow snowplow = new Snowplow();
 
-        // 1. A hókotróra hányó fej kerül
+        // 1. Előfeltétel: a hókotró aktuális feje a hányó fej
         snowplow.changeHead(throwerHead);
 
-        // 2. A takarító irányítja a hókotrót
-        cleanerRole.controlSnowplow(snowplow);
-
-        // 3. A hókotró megpróbálja letakarítani az útszakaszt
-        snowplow.clean(lane);
-
-        // 4. Egy szimulációs lépés végrehajtása opcionálisan
-        game.tick();
+        // 2. A takarító irányítja a hókotrót a tisztítandó sávra
+        cleanerRole.controlSnowplow(snowplow, lane);
 
         Skeleton.printReturn("");
     }
