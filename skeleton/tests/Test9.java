4b70343857a1f3fcdc5cfa41cee29e3a25c3cba4
Tóth Márton <toth.marton21@gmail.com>
2026-03-29 23:05:07 +0200

    Test9 final fixed

--- skeleton/tests/Test9.java
+++ skeleton/tests/Test9.java
@@ -3,32 +3,30 @@ package skeleton.tests;
 import skeleton.src.*;
 
 /**
- * 9. teszteset: Takarítás jégtörő fejjel teszt.
- *
- * A teszteset ellenőrzi, hogy a hókotró jégtörő fejjel
- * megfelelően takarítja az utat.
+ * 9. teszteset: Takarítás jégtörő fejjel teszt
+ * A teszteset ellenőrzi, hogy a hókotró jégtörő fejjel megfelelően takarítja az utat.
  */
 public class Test9 implements TestCase {
 
     /**
      * A tesztszekvencia futtatása.
-     *
-     * @param scanner a felhasználói bemenet olvasására szolgáló objektum
+     * Inicializálja a takarítót, a hókotrót a jégtörő fejjel, valamint egy útsávot.
      */
     @Override
     public void run() {
-
         Skeleton.printCall("Test9", "run()");
 
         CleanerRole cleanerRole = new CleanerRole();
-        Lane lane = new Lane();
-        IcebreakerHead iceBreakerHead = new IcebreakerHead();
         Snowplow snowplow = new Snowplow();
+        Lane targetLane = new Lane();
+        snowplow.changeLane(targetLane);
 
-        // 1. Előfeltétel: a hókotró aktuális feje a jégtörő fej
-        snowplow.changeHead(iceBreakerHead);
+        IcebreakerHead icebreakerHead = new IcebreakerHead();
 
-        // 2. A takarító a hókotrót a tisztítandó sávra irányítja
+        // Előfeltétel: A hókotró feje jégtörő fej
+        snowplow.changeHead(icebreakerHead);
+
+        // A takarítási folyamat indítása
         cleanerRole.controlSnowplow(snowplow);
 
         Skeleton.printReturn("");
