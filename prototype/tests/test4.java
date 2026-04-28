daa8e273fc6a0ee7cedd2160f4faf44147f094a1
Tóth Márton <toth.marton21@gmail.com>
2026-04-28 22:48:38 +0200

    Tests 4,5,6,7 added

--- prototype/tests/test4.java
+++ prototype/tests/test4.java
@@ -6,18 +6,21 @@ public class test4 implements TestCase {
     public void run() {
         System.out.println("=== Teszteset 4: Vásárlás a boltban ===");
 
-        Snowplow plow = new Snowplow("plow_1", null, 0, new SweeperHead());
-        CleanerRole cleaner = new CleanerRole("cleaner_1", 100, plow);
-        Store store = new Store();
+        // Bemenet szimulálása
+        System.out.println("load test4_arrange.txt");
+        System.out.println("bolt_nyit");
+        System.out.println("vasarol salt 3");
+        System.out.println("save test4_out.txt");
+        System.out.println("exit");
 
+        System.out.println();
+        System.out.println("--- Elvárt kimenet ---");
+
+        // Elvárt kimenet generálása a dokumentum alapján
         System.out.println("[store] [open]: false -> true");
         System.out.println("[store] [selectedItem]: null -> salt");
-
-        cleaner.addSalt(3);
-        cleaner.decreaseMoney(30);
-
-        System.out.println("[cleaner_1] [money]: 100 -> " + cleaner.getMoney());
-        System.out.println("[plow_1] [saltStock]: 0 -> " + plow.getSaltStock());
+        System.out.println("[cleaner_1] [money]: 100 -> 70");
+        System.out.println("[plow_1] [saltStock]: 0 -> 3");
         System.out.println("[log] Vásárlás sikeres: salt x3");
     }
 }
--- prototype/tests/test7.java
+++ prototype/tests/test7.java
@@ -6,31 +6,23 @@ public class test7 implements TestCase {
     public void run() {
         System.out.println("=== Teszteset 7: Autók automatikus közlekedése lakhely és munkahely között ===");
 
-        Residence home = new Residence("home_1");
-        Workplace work = new Workplace("work_1");
+        // Bemenet szimulálása
+        System.out.println("load test7_arrange.txt");
+        System.out.println("tick");
+        System.out.println("tick");
+        System.out.println("tick");
+        System.out.println("save test7_out.txt");
+        System.out.println("exit");
 
-        Lane laneHome = new Lane("lane_home", home, null);
-        Lane laneMid = new Lane("lane_mid", null, null);
-        Lane laneWork = new Lane("lane_work", null, work);
+        System.out.println();
+        System.out.println("--- Elvárt kimenet ---");
 
-        Car car = new Car("car_1", laneHome, 50.0, home, work);
-
-        Route route = new Route("route_car_1");
-        route.addLane(laneMid);
-        route.addLane(laneWork);
-
+        // Elvárt kimenet generálása a dokumentum alapján
         System.out.println("[car_1] [residence]: home_1");
         System.out.println("[car_1] [workplace]: work_1");
-
-        car.setCurrentRoute(route);
         System.out.println("[car_1] [currentRoute]: null -> route_car_1");
-
-        car.changeLane(laneMid);
         System.out.println("[car_1] [currentLane]: lane_home -> lane_mid");
-
-        car.changeLane(laneWork);
         System.out.println("[car_1] [currentLane]: lane_mid -> lane_work");
-
         System.out.println("[car_1] [location]: úton -> work_1");
         System.out.println("[log] car_1 megérkezett a munkahelyre");
     }
